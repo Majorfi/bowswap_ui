d9ca26f3d76a9710c373ecd9ce6145eb1aeeaca9
Thomas Bouder <tbouder@protonmail.com>
2021-08-02 10:41:59 +0200

    ✏️ Pandaswap to Bowswap

--- pages/_app.js
+++ pages/_app.js
@@ -22,11 +22,11 @@ function	AppWrapper(props) {
 	return (
 		<>
 			<Head>
-				<title>{'Pandaswap'}</title>
-				<link rel={'icon'} href={'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐼</text></svg>'} />
+				<title>{'Bowswap'}</title>
+				<link rel={'icon'} href={'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏹</text></svg>'} />
 				<meta httpEquiv={'X-UA-Compatible'} content={'IE=edge'} />
 				<meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
-				<meta name={'description'} content={'Pandaswap - Swap from one crv vault to an other'} />
+				<meta name={'description'} content={'Bowswap - Swap from one crv vault to an other'} />
 				<meta name={'msapplication-TileColor'} content={'#9fcc2e'} />
 				<meta name={'theme-color'} content={'#ffffff'} />
 				<meta charSet={'utf-8'} />
