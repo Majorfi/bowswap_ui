48ff5f42f31e3100693f4959ee48ab0a704cea27
Thomas Bouder <tbouder@protonmail.com>
2021-08-02 14:56:23 +0200

    ✏️ char change to update vercel config

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,12 +18,12 @@ function	Navbar() {
 		for (let i = 0; i < str.length; i++) {
 			hash = str.charCodeAt(i) + ((hash << 5) - hash);
 		}
-		let colour = '#';
+		let color = '#';
 		for (let i = 0; i < 3; i++) {
 			let value = (hash >> (i * 8)) & 0xFF;
-			colour += ('00' + value.toString(16)).substr(-2);
+			color += ('00' + value.toString(16)).substr(-2);
 		}
-		return colour;
+		return color;
 	};
 
 	function	renderWalletButton() {
