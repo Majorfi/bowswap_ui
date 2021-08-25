1fc3bfb726707a52fc614118eba10f81b96578fb
Thomas Bouder <tbouder@protonmail.com>
2021-08-25 21:13:02 +0200

    🔍️ Adding seo stuff

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@
 
 import	React, {useState, useEffect}	from	'react';
 import	Head							from	'next/head';
+import	{DefaultSeo}					from	'next-seo';
 import	{Web3ReactProvider}				from	'@web3-react/core';
 import	{ethers}						from	'ethers';
 import	useSWR							from	'swr';
@@ -93,11 +94,9 @@ function	AppWrapper(props) {
 	return (
 		<>
 			<Head>
-				<title>{active && hasSecretCode ? 'Crossbowswap' : 'Bowswap'}</title>
 				<link rel={'icon'} href={'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏹</text></svg>'} />
 				<meta httpEquiv={'X-UA-Compatible'} content={'IE=edge'} />
 				<meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
-				<meta name={'description'} content={active && hasSecretCode ? 'Crossbowswap - Increase Your Yield with Yearn. With extra step.' : 'Bowswap - Increase Your Yield with Yearn'} />
 				<meta name={'msapplication-TileColor'} content={'#9fcc2e'} />
 				<meta name={'theme-color'} content={'#ffffff'} />
 				<meta charSet={'utf-8'} />
@@ -105,6 +104,32 @@ function	AppWrapper(props) {
 				<link rel={'preconnect'} href={'https://fonts.gstatic.com'} crossOrigin={'true'} />
 				<link href={'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'} rel={'stylesheet'} />
 			</Head>
+			<DefaultSeo
+				title={'Bowswap'}
+				defaultTitle={'Increase Your Yield with Yearn'}
+				description={'Increase Your Yield with Yearn'}
+				openGraph={{
+					type: 'website',
+					locale: 'en_US',
+					url: 'https://bowswap.finance',
+					site_name: 'Bowswap',
+					title: 'Bowswap',
+					description: 'Increase Your Yield with Yearn',
+					images: [
+						{
+							url: 'https://bowswap.finance/og-bowswap.png',
+							width: 1200,
+							height: 675,
+							alt: 'Bowswap Finance',
+						}
+					]
+				}}
+				twitter={{
+					handle: '@iearnfinance',
+					site: '@iearnfinance',
+					cardType: 'summary_large_image',
+				}} />
+
 			{router.asPath === '/' ?
 				<main id={'app'} className={'flex w-full h-full relative min-h-screen'} style={{background: '#F2F3F5', overflow: 'hidden'}}>
 					<div className={'w-full h-full'}>
