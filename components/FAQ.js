cfecf1e1eee64dde02c3c88bab086db3316ccfe9
Major-Eth <major-tom.eth@pm.me>
2021-11-11 19:07:58 +0100

    🎨 UI lineheight

--- components/FAQ.js
+++ components/FAQ.js
@@ -44,58 +44,58 @@ function	FAQ() {
 		<div className={'space-y-4 mt-6 w-full'}>
 			<h3 className={'mb-6 font-bold text-center'}>{'FAQ'}</h3>
 			<FAQElement label={'What is Bowswap?'}>
-				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm leading-4'}>
 					{'Bowswap is a platform that allows you to move your funds from one place to another using the most optimal route, to pursue better yield. You can either move them inside Yearn, or migrate them from other protocols.'}
 				</div>
 			</FAQElement>
 			<FAQElement label={'What are the benefits of using Bowswap?'}>
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'Using Bowswap you save time and, more importantly, money. It\'s time-saving and simple. Just one click and you migrate.'}
 				</div>
-				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm leading-4'}>
 					{'You save money in gas because Bowswap uses the most optimal route to migrate and sends all the transactions bundled.'}
 				</div>
 			</FAQElement>
 			<FAQElement label={'How does the swap work?'}>
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'Bowswap smart contracts withdraw from the protocol, or yVault, and find the most optimal route to go from one place to the other.'}
 				</div>
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'If you migrate from stable to stable, there are no swaps, just the automation of withdrawals and deposits into different protocols.'}
 				</div>
-				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm leading-4'}>
 					{'If you move from one type of asset to another one, there will be a swap using the best available route.'}
 				</div>
 			</FAQElement>
 			<FAQElement label={'I can\'t find the vault I want to migrate from/to, why?'}>
-				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm leading-4'}>
 					{'We want to make sure we can provide the best routes. Bowswap doesn\'t support every single vault yet. We are working hard to support more and more vaults every day.'}
 				</div>
 			</FAQElement>
 			<FAQElement label={'Are there any fees to use it?'}>
-				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm leading-4'}>
 					{'Bowswap charges no fees for using it. But you can donate a tiny portion of your transaction to help maintain the team.'}
 				</div>
 			</FAQElement>
 			<FAQElement label={'What is slippage?'}>
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'In trade, there is almost always a spread between the price that a buyer will pay and the price that a seller will sell an asset. When an order is made, this difference in price between buyer and seller expectations results in price slippage. This slippage in price is usually 1-3% but can be even more for coins with limited liquidity. This slippage can lead to a final sale price of the asset that is either more or less than the requested transaction amount.'}
 				</div>
-				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm leading-4'}>
 					{'If you are moving between the same type of asset, there will be no swap, so no slippage will apply.'}
 				</div>
 			</FAQElement>
 			<FAQElement label={'Why do I need MEV protection?'}>
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'MEV represents the extractable value via conventional or unconventional methods. The conventional ones, incentivize miners to run the network, while unconventional ones are used by bots to make a profit from you.'}
 				</div>
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'With MEV protection your transaction will not be seen by hungry sandwich bots in the public mempool.'}
 				</div>
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'If you are moving between the same type of asset, there will be no swap, so no slippage will apply.'}
 				</div>
-				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm leading-4'}>
 					{'Learn more about '}
 					<a href={'https://medium.com/flashbots/frontrunning-the-mev-crisis-40629a613752'} target={'_blank'} rel={'noreferrer'} className={'text-yblue hover:text-yblue-hover font-medium'}>{'MEV'}</a>
 					{'.'}
@@ -104,7 +104,7 @@ function	FAQ() {
 			<FAQElement label={'How to enable MEV protection?'}>
 				<div className={'mb-6 px-6'}>
 					<div className={'bg-ygray-100 rounded-lg p-4 relative overflow-hidden'}>
-						<div className={'font-normal text-ygray-700 text-sm w-full flex flex-row items-center'}>
+						<div className={'font-normal text-ygray-700 text-sm leading-4 w-full flex flex-row items-center'}>
 							<div className={'w-1/4'}>{'Network Name: '}</div>
 							<div
 								className={'cursor-pointer flex flex-row group items-center'}
@@ -118,7 +118,7 @@ function	FAQ() {
 								</div>
 							</div>
 						</div>
-						<div className={'font-normal text-ygray-700 text-sm w-full flex flex-row items-center'}>
+						<div className={'font-normal text-ygray-700 text-sm leading-4 w-full flex flex-row items-center'}>
 							<div className={'w-1/4'}>{'RPC URL: '}</div>
 							<div
 								className={'cursor-pointer flex flex-row group items-center'}
@@ -131,7 +131,7 @@ function	FAQ() {
 								</div>
 							</div>
 						</div>
-						<div className={'font-normal text-ygray-700 text-sm w-full flex flex-row items-center'}>
+						<div className={'font-normal text-ygray-700 text-sm leading-4 w-full flex flex-row items-center'}>
 							<div className={'w-1/4'}>{'Chain ID: '}</div>
 							<div
 								className={'cursor-pointer flex flex-row group items-center'}
@@ -144,7 +144,7 @@ function	FAQ() {
 								</div>
 							</div>
 						</div>
-						<div className={'font-normal text-ygray-700 text-sm w-full flex flex-row items-center'}>
+						<div className={'font-normal text-ygray-700 text-sm leading-4 w-full flex flex-row items-center'}>
 							<div className={'w-1/4'}>{'Currency Symbol: '}</div>
 							<div
 								className={'cursor-pointer flex flex-row group items-center'}
@@ -166,16 +166,16 @@ function	FAQ() {
 				</div>
 
 
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'To enable the MEV protection you have to add the Flashbots Protect RPC. It\'s a simple manual process in Metamask. Then you need to use it each time you do a swap on Ethereum. That way you will always be protected from hungry sandwich bots.'}
 				</div>
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'With MEV protection your transaction will not be seen by hungry sandwich bots in the public mempool.'}
 				</div>
-				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-2 font-normal text-ygray-700 text-sm leading-4'}>
 					{'If you are moving between the same type of asset, there will be no swap, so no slippage will apply.'}
 				</div>
-				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm'}>
+				<div className={'px-6 pb-6 font-normal text-ygray-700 text-sm leading-4'}>
 					{'You can follow the detailed '}
 					<a href={'https://docs.flashbots.net/flashbots-protect/rpc/quick-start/'} target={'_blank'} rel={'noreferrer'} className={'text-yblue hover:text-yblue-hover font-medium'}>{'instructions'}</a>
 					{'.'}
