ac24a6330fb125b57e6b0f391e5623caa3a42a72
Major-Eth <major-tom.eth@pm.me>
2021-12-16 11:30:54 +0100

    ✨ Adding API

---
+++ pages/api/path.js
@@ -0,0 +1,29 @@
+import	{toAddress}				from	'utils';
+import	METAPOOL_SWAPS			from	'utils/detected_metapoolSwaps';
+import	SWAPS					from	'utils/detected_swaps';
+
+export default async function handler(req, res) {
+	let		{from, to} = req.query;
+	if (!from || from === '' || !to || to === '') {
+		return res.status(200).json({});
+	}
+
+	from = toAddress(from);
+	to = toAddress(to);
+
+	const	metapoolSwap = METAPOOL_SWAPS.find(e => e[0] === from && e[1] === to);
+	if (metapoolSwap) {
+		return res.status(200).json({
+			isMetapoolSwap: true,
+			data: metapoolSwap,
+		});
+	}
+	const	swap = SWAPS.find(e => e[0] === from && e[1] === to);
+	if (swap) {
+		return res.status(200).json({
+			isMetapoolSwap: false,
+			data: swap,
+		});
+	}
+	return res.status(200).json({});
+}
